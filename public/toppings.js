710e31eb5e265e776caef2e4d4accf76965b6120
Kevin Nolan <betaalpha@yahoo.com>
2017-03-06 16:45:20 +0000

    Tying the two programs together

---
+++ public/toppings.js
@@ -0,0 +1,36 @@
+const topps = {
+  ham: {dist:[0,3,6,7], dRot:0.1,  src: ["piz_ing_ham.svg"], obj:[], interv:0 },
+  bacon: {dist:[1,3,5], dRot:0.2,    src: ["piz_ing_bacon.svg"], obj:[], inter:0},
+  pepperoni: {dist:[0,3,6,7], dRot:0.5,  src: ["piz_ing_pepperoni.svg"], obj:[], interv:0 },
+  salami: {dist:[1,3,5], dRot:0.75,  src: ["piz_ing_salami.svg"], obj:[], interv:0 },
+  shrimps: {dist:[0,3,6,7], dRot:0.3,  src: ["piz_ing_shrimp.svg"], obj:[], interv:0 },
+  mushrooms: {dist:[1,3,6,7], dRot:0.4,  src: ["piz_ing_mushroom.svg"], obj:[], interv:0 },
+  tomato: {dist:[1,3,6,7], dRot:0.1,  src: ["piz_ing_tomato.svg"], obj:[], interv:0 },
+  pineapple: {dist:[1,0,6], dRot:0.1,  src: ["piz_ing_pineapple.svg"], obj:[], interv:0 },
+  redonion: {dist:[0,3,7], dRot:0.6,  src: ["piz_ing_redonion.svg"], obj:[], interv:0 },
+  jalapenos: {dist:[0,3,6], dRot:0.7,    src: ["piz_ing_greenchilli.svg", "piz_ing_redchilli.svg"], obj:[], inter:0},
+  peppers: {dist:[0,3,7], dRot:0.8,  src: ["piz_ing_greenpepper.svg", "piz_ing_redpepper.svg", "piz_ing_yellowpepper.svg"], obj:[], interv:0 },
+  olives: {dist:[1,3,6,7], dRot:0.25,  src: ["piz_ing_greenolive.svg", "piz_ing_blackolive.svg"], obj:[], interv:0 },
+  basil: {dist:[0,2,5], dRot:0.35,  src: ["piz_ing_basil.svg"], obj:[], interv:0 },
+  parsley: {dist:[0,2,5], dRot:0.75,  src: ["piz_ing_parsley.svg"], obj:[], interv:0 },
+  groundchili: {dist:[1,4,12,20], dRot:0, src: ["piz_ing_redgroundc.svg", "piz_ing_yellowgroundc.svg", "piz_ing_orangegroundc.svg"], obj:[], inter:0},
+};
+
+const setToppings = { // Toppings are dropped in order onto pizza, so seasonings should go last.
+margherita:  {name: "Margherita", ingred: []},
+hawaiian:    {name: "Hawaiian", ingred: ["ham", "pineapple"]},
+surfnturf:   {name: "Surf n' Turf", ingred: ["bacon", "shrimps"]},
+farmhouse:   {name: "Farmhouse", ingred: ["ham", "mushrooms","tomato"]},
+meatfeast:   {name: "Meat Feast", ingred: ["bacon", "ham","pepperoni"]},
+vegetarian:  {name: "Vegetarian", ingred: ["mushrooms", "peppers","redonion","tomato"]},
+sizzler:     {name: "Sizzler", ingred: ["jalapenos", "pepperoni","peppers","redonion"]},
+supreme:     {name: "Supreme", ingred: ["bacon", "ham","mushrooms","olives","pepperoni","tomato","basil"]},
+vegsupreme:  {name: "Veg Supreme", ingred: ["mushrooms","olives","peppers","redonion","tomato","parsley","basil"]}
+};
+
+// Required for EJS to use these variables via nodeJS
+
+module.exports = {
+  topps: topps,
+  setToppings: setToppings
+};
--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const express = require('express');
 const bodyParser= require('body-parser')
 const app = express();
 const MongoClient = require('mongodb').MongoClient
+const pubPath = __dirname + '\\public';
 // This system uses MongoLab - https://mlab.com
-app.use(bodyParser.urlencoded({extended: true}))
-app.set('view engine', 'ejs') // EJS is a simple template engine for rendering the quotes to screen
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.set('view engine', 'ejs'); // EJS is a simple template engine for rendering the quotes to screen
 
 let db;
 
@@ -17,23 +19,42 @@ MongoClient.connect(process.env.DATABASE_URL, (err,database) => {
   });
 });
 
-app.get('/', (req, res) => {
-  db.collection('pizza').find().toArray(function(err,pResult) {
+app.get('/', (req,res) => {
+  res.sendFile(pubPath+'/index.html');
+})
+
+app.get('/vote', (req, res) => {
+  let toppFile = require(pubPath+'/toppings.js');
+
+  db.collection('pizzas').find().toArray(function(err,pResult) {
     if (err) return console.log('Pizza collection get error',err);
     db.collection('newtoppings').find().toArray(function(err,nTResult) {
       if (err) return console.log('newtoppings collection get error', err);
       //renders index.ejs
-      res.render('index.ejs', {pizza:pResult,newToppings:nTResult});
-    })
+      res.render('vote.ejs', {
+        topps:toppFile.topps,
+        setToppings: toppFile.setToppings,
+        pizzas:pResult,
+        newToppings:nTResult
+      });
+    });
   });
 });
 
-app.post('/pizza', (req,res) => {
-  db.collection('pizza').save(req.body, (err,result) =>{
+app.post('/newtoppings', (req,res) => {
+  db.collection('newtoppings').save(req.body, (err,result) =>{
     if (err) return console.log(err)
 
-    console.log('saved to database')
-    res.redirect('/')
+    console.log('saved to newtoppings collection')
+    res.redirect('/vote')
   });
 });
 
+app.post('/pizzas', (req,res) => {
+  db.collection('pizzas').save(req.body, (err,result) =>{
+    if (err) return console.log(err)
+
+    console.log('saved to pizzas collection')
+    res.redirect('/vote')
+  });
+});
